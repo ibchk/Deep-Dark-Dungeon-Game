386ee5544f8640b709028d40f362b1095d67fb4a
ilboit <ilboit@ttu.ee>
2020-02-19 13:40:02 +0200

    game 0.1

---
+++ desktop/src/ee/taltech/deepdarkdungeon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ee.taltech.deepdarkdungeon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ee.taltech.deepdarkdungeon.DeepDarkDungeonGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DeepDarkDungeonGame(), config);
+	}
+}
