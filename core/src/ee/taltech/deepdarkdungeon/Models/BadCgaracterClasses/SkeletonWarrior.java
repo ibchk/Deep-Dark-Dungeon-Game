8c657d2805965a2443ef16d0c0277432c8496696
arthur <arthurp200148@gmail.com>
2020-02-29 12:58:38 +0200

    Monster classes are almost ready!

---
+++ core/src/ee/taltech/deepdarkdungeon/Models/BadCgaracterClasses/SkeletonWarrior.java
@@ -0,0 +1,75 @@
+package ee.taltech.deepdarkdungeon.Models.BadCgaracterClasses;
+
+import com.badlogic.gdx.graphics.Texture;
+import ee.taltech.deepdarkdungeon.Models.GameObject;
+
+public class SkeletonWarrior extends GameObject {
+
+    String name;
+    int health;
+    int power;
+    double x;
+    double y;
+    double width;
+    double height;
+    BadCharacterClass badCharacterClass;
+    CharacterType characterType;
+    Texture picture;
+
+    public SkeletonWarrior (int health, int power, double x, double y, double width, double height, BadCharacterClass badCharacterClass, CharacterType characterType) {
+        this.name = "Skeleton Warrior";
+        this.health = 50;
+        this.power = 10;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.badCharacterClass = badCharacterClass;
+        this.characterType = characterType;
+    }
+
+    @Override
+    public Texture getPicture() {
+        return null;
+    }
+
+    @Override
+    public CharacterType getCharacterType() {
+        return null;
+    }
+
+    @Override
+    public double getX() {
+        return 0;
+    }
+
+    @Override
+    public double getY() {
+        return 0;
+    }
+
+    @Override
+    public double getWidth() {
+        return 0;
+    }
+
+    @Override
+    public double getHeight() {
+        return 0;
+    }
+
+    @Override
+    public int getPower() {
+        return 0;
+    }
+
+    @Override
+    public int getHealth() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
--- core/src/ee/taltech/deepdarkdungeon/Models/GameObject.java
+++ core/src/ee/taltech/deepdarkdungeon/Models/GameObject.java
@@ -3,6 +3,10 @@ package ee.taltech.deepdarkdungeon.Models;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import ee.taltech.deepdarkdungeon.Models.BadCgaracterClasses.Necromancer;
+import ee.taltech.deepdarkdungeon.Models.BadCgaracterClasses.SkeletonArcher;
+import ee.taltech.deepdarkdungeon.Models.BadCgaracterClasses.SkeletonWarrior;
+import ee.taltech.deepdarkdungeon.Models.BadCgaracterClasses.Zombie;
 import ee.taltech.deepdarkdungeon.Models.characterClasses.Archer;
 import ee.taltech.deepdarkdungeon.Models.characterClasses.Magic;
 import ee.taltech.deepdarkdungeon.Models.characterClasses.Paladin;
@@ -34,17 +38,12 @@ public abstract class GameObject {
     public abstract String getName();
 
     public enum CharacterClass {PALADIN, WARIOR, MAGIC, ARCHER}
+    public enum BadCharacterClass {ZOMBIE, SKELETON_WARRIOR, SKELETON_ARCHER, NECROMANCER}
     public enum CharacterType {GOOD1, GOOD2, GOOD3, GOOD4, BAD1, BAD2, BAD3, BAD4}
 
     public GameObject createCharacter(Texture texture, String name, CharacterType characterType, CharacterClass characterClass, int power, int health, double x, double y, double width, double height) {
         GameObject object = null;
         switch (characterType) {
-            case BAD1:
-            case BAD2:
-            case BAD3:
-            case BAD4:
-                object = new BadCharacter(texture, name, characterType, power, health, x, y, width, height);
-                break;
             case GOOD1:
             case GOOD2:
             case GOOD3:
@@ -60,6 +59,34 @@ public abstract class GameObject {
                 }
                 if (characterClass == CharacterClass.ARCHER) {
                     object = new Archer(name, health, power, x, y, width, height, characterClass, characterType);
+                }
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + characterType);
+        }
+        this.bounds = new Rectangle((int) x, (int) y, (int) width, (int) height);
+        this.object = new Sprite(getPicture());
+        return object;
+    }
+
+    public GameObject createBadCharacter(Texture texture, String name, CharacterType characterType, BadCharacterClass badCharacterClass, int power, int health, double x, double y, double width, double height) {
+        GameObject object = null;
+        switch (characterType) {
+            case BAD1:
+            case BAD2:
+            case BAD3:
+            case BAD4:
+                if (badCharacterClass == BadCharacterClass.ZOMBIE) {
+                    object = new Zombie(health, power, x, y, width, height, badCharacterClass, characterType);
+                }
+                if (badCharacterClass == BadCharacterClass.SKELETON_WARRIOR) {
+                    object = new SkeletonWarrior(health, power, x, y, width, height, badCharacterClass, characterType);
+                }
+                if (badCharacterClass == BadCharacterClass.SKELETON_ARCHER) {
+                    object = new SkeletonArcher(health, power, x, y, width, height, badCharacterClass, characterType);
+                }
+                if (badCharacterClass == BadCharacterClass.NECROMANCER) {
+                    object = new Necromancer(health, power, x, y, width, height, badCharacterClass, characterType);
                 }
                 break;
             default:
