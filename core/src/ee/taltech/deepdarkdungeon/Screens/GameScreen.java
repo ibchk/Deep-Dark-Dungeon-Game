cc6b0ae5a57001dde9a9d1015b4ddcbc724a57cf
nikit <nibirj@ttu.ee>
2020-03-01 16:10:17 +0100

    Fix

--- core/src/ee/taltech/deepdarkdungeon/Screens/GameScreen.java
+++ core/src/ee/taltech/deepdarkdungeon/Screens/GameScreen.java
@@ -19,6 +19,7 @@ public class GameScreen implements Screen {
     private static final int VBOI_WIDTH = 201;
 
     DeepDarkDungeonGame game;
+    boolean canbeattacked = false;
     BitmapFont font = new BitmapFont();
     private long stepCount = 1;
     private Texture vboiButton;
@@ -67,17 +68,54 @@ public class GameScreen implements Screen {
         batch.draw(badCharacter2.getTexture(), 1300, 100);
         batch.draw(badCharacter3.getTexture(), 1500, 100);
         batch.draw(badCharacter4.getTexture(), 1700, 100);
+        font.draw(batch, badCharacter1.getHealth() + "", 1200, 400);
+        font.draw(batch, badCharacter2.getHealth() + "", 1400, 400);
+        font.draw(batch, badCharacter3.getHealth() + "", 1600, 400);
+        font.draw(batch, badCharacter4.getHealth() + "", 1800, 400);
         //Gdx.input.getX() < PLAY_BUTTON_START + PLAY_BUTTON_WIDTH && Gdx.input.getX() > PLAY_BUTTON_START && DeepDarkDungeonGame.HEIGHT - Gdx.input.getY() < PLAY_BUTTON_Y + PLAY_BUTTON_HEIGHT && DeepDarkDungeonGame.HEIGHT - Gdx.input.getY() > PLAY_BUTTON_Y)
         if (stepCount % 2 == 1) {
             font.draw(batch, "Your turn!" + stepCount, 100, 1000); // Вызывает текст, тут например power персанажа
             if (Gdx.input.getX() < VBOI_X + VBOI_WIDTH && Gdx.input.getX() > VBOI_X && DeepDarkDungeonGame.HEIGHT - Gdx.input.getY() <= VBOI_Y + VBOI_HEIGTH && DeepDarkDungeonGame.HEIGHT - Gdx.input.getY() >= VBOI_Y) {
                 if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
-                    stepCount += 1;
+                    batch.draw(goodCharacter2.getTexture(), 200, 800);
+                    canbeattacked = true;
                 }
             }
         } else if (stepCount % 2 == 0) {
             font.draw(batch, "Monsters turn!" + stepCount, 100, 900); // Вызывает текст, тут например power персанажа
             stepCount += 1;
+        }
+        if (Gdx.input.getX() > badCharacter1.getX() && Gdx.input.getX() < badCharacter1.getX() + 200 && DeepDarkDungeonGame.HEIGHT - Gdx.input.getY() > badCharacter1.getY() && DeepDarkDungeonGame.HEIGHT - Gdx.input.getY() < badCharacter1.getY() + 300) {
+            batch.draw(goodCharacter1.getTexture(), 200, 500);
+            if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT) && canbeattacked) {
+                badCharacter1.setHealth(badCharacter1.getHealth() - goodCharacter1.getPower());
+                stepCount += 1;
+                canbeattacked = false;
+            }
+        }
+        if (Gdx.input.getX() > badCharacter2.getX() && Gdx.input.getX() < badCharacter2.getX() + 200 && DeepDarkDungeonGame.HEIGHT - Gdx.input.getY() > badCharacter2.getY() && DeepDarkDungeonGame.HEIGHT - Gdx.input.getY() < badCharacter2.getY() + 300) {
+            batch.draw(goodCharacter2.getTexture(), 200, 500);
+            if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT) && canbeattacked) {
+                badCharacter2.setHealth(badCharacter2.getHealth() - goodCharacter1.getPower());
+                stepCount += 1;
+                canbeattacked = false;
+            }
+        }
+        if (Gdx.input.getX() > badCharacter3.getX() && Gdx.input.getX() < badCharacter3.getX() + 200 && DeepDarkDungeonGame.HEIGHT - Gdx.input.getY() > badCharacter3.getY() && DeepDarkDungeonGame.HEIGHT - Gdx.input.getY() < badCharacter3.getY() + 300) {
+            batch.draw(goodCharacter3.getTexture(), 200, 500);
+            if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT) && canbeattacked) {
+                badCharacter3.setHealth(badCharacter3.getHealth() - goodCharacter1.getPower());
+                stepCount += 1;
+                canbeattacked = false;
+            }
+        }
+        if (Gdx.input.getX() > badCharacter4.getX() && Gdx.input.getX() < badCharacter4.getX() + 200 && DeepDarkDungeonGame.HEIGHT - Gdx.input.getY() > badCharacter4.getY() && DeepDarkDungeonGame.HEIGHT - Gdx.input.getY() < badCharacter4.getY() + 300) {
+            batch.draw(goodCharacter4.getTexture(), 200, 500);
+            if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT) && canbeattacked) {
+                badCharacter4.setHealth(badCharacter4.getHealth() - goodCharacter1.getPower());
+                stepCount += 1;
+                canbeattacked = false;
+            }
         }
         batch.end();
     }
